2499c95ccbdf81cb24f5396e4337a6c7a6e1d729
孙成 <suncheng@dasmaster.com>
2015-11-25 21:30:53 +0800

    add a log frame--sfl4j&&logback

---
+++ container/src/main/java/com/nevermore/main/Bootstrap.java
@@ -0,0 +1,17 @@
+package com.nevermore.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author suncheng
+ * @version 0.0.1
+ * @since 15/11/25
+ */
+public class Bootstrap {
+    private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
+
+    public static void main(String args[]) {
+        logger.info("aaaa");
+    }
+}
